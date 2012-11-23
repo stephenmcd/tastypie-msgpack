a822f59b3df9ff9963fce65727f6df2393b61312
Stephen McDonald <steve@jupo.org>
2012-11-23 22:57:21 +1100

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tastypie-msgpack",
-    version="0.0.1",
+    version="0.0.2",
     author="Stephen McDonald",
     author_email="stephen.mc@gmail.com",
     description="MsgPack support for Django Tastypie.",
